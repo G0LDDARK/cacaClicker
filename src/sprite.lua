51c2868d8b002aa2aaa4bcc492a47b0185a5cffc
Kaiwinta <titouan.pradoura@epitech.eu>
2025-04-08 17:06:20 +0200

    [feat]: Base hitbox alpha handling

--- src/sprite.lua
+++ src/sprite.lua
@@ -26,8 +26,14 @@ function Sprite:draw()
 end
 
 function Sprite:isHovered(mx, my)
-    return mx >= self.x and mx <= self.x + self.width and
-           my >= self.y and my <= self.y + self.height
+    if ( mx >= self.x and mx <= self.x + self.width * self.scaleX and
+           my >= self.y and my <= self.y + self.height * self.scaleY ) 
+    then
+        local r, g, b, a = self.image.img:getData():getPixel(mx, my)
+        return a > 0
+    else
+        return false
+    end
 end
 
 return Sprite
