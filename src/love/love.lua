b857bc64d57ec6a5f4fa7a20a91aaacc4ff07718
Raphaël <raphael.petrequin@epitech.eu>
2025-04-15 17:01:53 +0200

    add: musics and sounds

--- src/love/love.lua
+++ src/love/love.lua
@@ -9,10 +9,19 @@ local TiledSprite = require("src.tiledSprite")
 
 local background
 local poopSprite
+local music
+local clickSound
 
 function love.load()
     background = TiledSprite.new(config.backgrounds.main)
     poopSprite = Sprite.new(config.sprites.poop)
+
+    music = love.audio.newSource("assets/musics/mainMusic.ogg", "stream")
+    music:setLooping(true)     -- Loop the music
+    music:setVolume(0.5)       -- Set volume (0.0 to 1.0)
+    music:play()
+
+    clickSound = love.audio.newSource("assets/sounds/click.ogg", "static")
 end
 
 function love.update(dt)
@@ -26,6 +35,8 @@ end
 function love.mousepressed(x, y, button)
     if button == 1 and poopSprite:isHovered(x, y) then
         print("Poop clicked!")
+        if clickSound:isPlaying() then clickSound:stop() end
+        clickSound:play()
     end
 end
 
